3881bcdd72409e49d5a57e24b0e62135ef451844
Payal Dilip Ashtikar <payalashtikar@gmail.com>
2022-03-29 15:00:05 +0530

    Assessment-1 submission

---
+++ Assignment-1/code/src/components/AppComponentColor.js
@@ -0,0 +1,11 @@
+
+
+function AppComponentColor({children}) {
+    return (<div className="AppComponentColor">
+        {children}
+      </div>
+    );
+  }
+  
+export default AppComponentColor;
+  
