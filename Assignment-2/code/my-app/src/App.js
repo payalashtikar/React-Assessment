2eb8a1b6790ff4ccc42c0e2d7e1f42ef2b530a5d
Payal Dilip Ashtikar <payalashtikar@gmail.com>
2022-03-31 12:32:02 +0530

    Assignment-2 submission

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 
@@ -9,6 +9,19 @@ function App(props) {
 return(
   <div className="container">
   {/* Start your React code here */}
+  <img src = {employee.profileImg}/>
+
+  <h1 className="empname">{employee.name}</h1>
+
+  <p className='loc'>Location</p>
+  <h4 className='data'>{employee.location}</h4>
+
+  <p className='info'>Blood group</p>
+  <h4 className='data'>{employee.bloodGroup}</h4>
+
+  <p className='info'>Age</p>
+  <h4 className='data'>{employee.age}</h4>
+
 </div>
 )
 }
